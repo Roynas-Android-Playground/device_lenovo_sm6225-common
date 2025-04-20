f636e801faf4760f3e2fab7b18719a2ae5fe786c
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-20 11:55:02 +0900

    sm6225-common: Setup for elf checks
    Squash of:
    
    sm6225-common: Add vendor soong imports to extract
    
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a
    
    sm6225-common: fix com.qualcomm.qti.dpm.api@1.0 ELF checks
    
    Change-Id: I759752ccc133d7047d415eb83c5d3c55820437a4
    
    sm6225-common: fix vendor.qti.hardware.fm@1.0 ELF checks
    
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d
    
    sm6225-common: fix vendor.qti.imsrtpservice@3.0 ELF checks
    
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09
    
    sm6225-common: skip dependencies which aren't defined in .bp
    
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,6 +21,39 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+                "device/lenovo/sm6225-common",
+                "hardware/qcom-caf/sm8250",
+                "hardware/qcom-caf/wlan",
+                "vendor/qcom/opensource/dataservices",
+                "vendor/qcom/opensource/display",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.dpm.api@1.0 | vendor.qti.hardware.fm@1.0 | vendor.qti.imsrtpservice@3.0)
+            echo "$1_vendor"
+            ;;
+        libqsap_sdk | libril | libwpa_client)
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
